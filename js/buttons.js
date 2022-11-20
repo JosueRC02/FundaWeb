d201046ecde0e858be249f8fc2bc4d7678370a3c
JosueRC02 <jretanacespedes@gmail.com>
2022-11-20 13:10:12 -0600

    pruebajs

--- js/buttons.js
+++ js/buttons.js
@@ -1,5 +1,14 @@
-import Swal from 'sweetalert2/dist/sweetalert2.js'
+const Swal = require('../node_modules/sweetalert2/dist/sweetalert2.all.min');
+
+let verde = document.getElementById("verde");
+
+verde.addEventListener("click", btnverde);
 
 function btnverde(){
-    console.log("hola");
+    Swal.fire({
+    title: 'Error!',
+    text: 'Do you want to continue',
+    icon: 'error',
+    confirmButtonText: 'Cool'
+    })
 }
