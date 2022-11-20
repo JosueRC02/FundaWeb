46f6e00d6615ef881f0853a8a9a68d20053867ef
JosueRC02 <jretanacespedes@gmail.com>
2022-11-20 17:00:33 -0600

    arreglo

--- js/buttons.js
+++ js/buttons.js
@@ -1,8 +1,49 @@
+//Boton Verde
 $(window).load(function () {
     $(".button1").click(function () {
-        $('.hover_bkgr_fricc').show();
+        $('.hover_bkgr_fricc1').show();
     });
-    $('.popupCloseButton').click(function () {
-        $('.hover_bkgr_fricc').hide();
+    $('.popupCloseButton1').click(function () {
+        $('.hover_bkgr_fricc1').hide();
+    });
+});
+
+//Boton Azul
+$(window).load(function () {
+    $(".button2").click(function () {
+        $('.hover_bkgr_fricc2').show();
+    });
+    $('.popupCloseButton2').click(function () {
+        $('.hover_bkgr_fricc2').hide();
+    });
+});
+
+//Boton Azul
+$(window).load(function () {
+    $(".button3").click(function () {
+        $('.hover_bkgr_fricc3').show();
+    });
+    $('.popupCloseButton3').click(function () {
+        $('.hover_bkgr_fricc3').hide();
+    });
+});
+
+//Boton Gris
+$(window).load(function () {
+    $(".button4").click(function () {
+        $('.hover_bkgr_fricc4').show();
+    });
+    $('.popupCloseButton4').click(function () {
+        $('.hover_bkgr_fricc4').hide();
+    });
+});
+
+//Boton Negro
+$(window).load(function () {
+    $(".button5").click(function () {
+        $('.hover_bkgr_fricc5').show();
+    });
+    $('.popupCloseButton5').click(function () {
+        $('.hover_bkgr_fricc5').hide();
     });
 });
