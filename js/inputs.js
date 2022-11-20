89378814ea282e3053b958b62d9ff3a1485a73df
Michael_Solis <michael.solis.alvarado@est.una.ac.cr>
2022-11-20 17:40:42 -0600

    tablas

---
+++ js/inputs.js
@@ -0,0 +1,8 @@
+$(window).load(function () {
+    $(".imputNormal").click(function () {
+        $('.ventana').show();
+    });
+    $('.popup').click(function () {
+        $('.ventana').hide();
+    });
+});
---
+++ js/tables.js
@@ -0,0 +1,18 @@
+//Boton Verde
+$(window).load(function () {
+    $(".table").click(function () {
+        $('.tablaventana').show();
+    });
+    $('.tablapopup').click(function () {
+        $('.tablaventana').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".table1").click(function () {
+        $('.tablaventana1').show();
+    });
+    $('.tablapopup1').click(function () {
+        $('.tablaventana1').hide();
+    });
+});
