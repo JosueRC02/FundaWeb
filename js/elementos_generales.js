74f236d7d987e43eb2fbb6c9dd10ee740b8f6e11
Michael_Solis <michael.solis.alvarado@est.una.ac.cr>
2022-11-20 22:02:03 -0600

    elementosGenerales

---
+++ js/elementos_generales.js
@@ -0,0 +1,71 @@
+$(window).load(function () {
+    $(".tabE").click(function () {
+        $('.eleventana').show();
+    });
+    $('.elepopup').click(function () {
+        $('.eleventana').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".listaE").click(function () {
+        $('.eleventana1').show();
+    });
+    $('.elepopup1').click(function () {
+        $('.eleventana1').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".toggleE").click(function () {
+        $('.eleventana2').show();
+    });
+    $('.elepopup2').click(function () {
+        $('.eleventana2').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".menuE").click(function () {
+        $('.eleventana3').show();
+    });
+    $('.elepopup3').click(function () {
+        $('.eleventana3').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".scrollE").click(function () {
+        $('.eleventana4').show();
+    });
+    $('.elepopup4').click(function () {
+        $('.eleventana4').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".CascadingE").click(function () {
+        $('.eleventana5').show();
+    });
+    $('.elepopup5').click(function () {
+        $('.eleventana5').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".cartaE").click(function () {
+        $('.eleventana6').show();
+    });
+    $('.elepopup6').click(function () {
+        $('.eleventana6').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".paginacionE").click(function () {
+        $('.eleventana7').show();
+    });
+    $('.elepopup7').click(function () {
+        $('.eleventana7').hide();
+    });
+});
