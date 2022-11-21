a978baf50e22cf4474ea1945ce7859f5eb85e809
Michael_Solis <michael.solis.alvarado@est.una.ac.cr>
2022-11-20 20:07:43 -0600

    inputsCasi

--- js/inputs.js
+++ js/inputs.js
@@ -1,8 +1,169 @@
 $(window).load(function () {
     $(".imputNormal").click(function () {
-        $('.ventana').show();
+        $('.inputventana').show();
     });
-    $('.popup').click(function () {
-        $('.ventana').hide();
+    $('.inputpopup').click(function () {
+        $('.inputventana').hide();
+    });
+});
+$(window).load(function () {
+    $(".imputDisable").click(function () {
+        $('.inputventana1').show();
+    });
+    $('.inputpopup1').click(function () {
+        $('.inputventana1').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".readOnly").click(function () {
+        $('.inputventana2').show();
+    });
+    $('.inputpopup2').click(function () {
+        $('.inputventana2').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".textArea").click(function () {
+        $('.inputventana3').show();
+    });
+    $('.inputpopup3').click(function () {
+        $('.inputventana3').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".dateOf").click(function () {
+        $('.inputventana4').show();
+    });
+    $('.inputpopup4').click(function () {
+        $('.inputventana4').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".pass").click(function () {
+        $('.inputventana5').show();
+    });
+    $('.inputpopup5').click(function () {
+        $('.inputventana5').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".selectI").click(function () {
+        $('.inputventana6').show();
+    });
+    $('.inputpopup6').click(function () {
+        $('.inputventana6').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".selectC").click(function () {
+        $('.inputventana7').show();
+    });
+    $('.inputpopup7').click(function () {
+        $('.inputventana7').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".selectG").click(function () {
+        $('.inputventana8').show();
+    });
+    $('.inputpopup8').click(function () {
+        $('.inputventana8').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".selectM").click(function () {
+        $('.inputventana9').show();
+    });
+    $('.inputpopup9').click(function () {
+        $('.inputventana9').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".inputtag").click(function () {
+        $('.inputventana10').show();
+    });
+    $('.inputpopup10').click(function () {
+        $('.inputventana10').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".imputDisable").click(function () {
+        $('.inputventana11').show();
+    });
+    $('.inputpopup11').click(function () {
+        $('.inputventana11').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".imputDisable").click(function () {
+        $('.inputventana12').show();
+    });
+    $('.inputpopup12').click(function () {
+        $('.inputventana12').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".imputDisable").click(function () {
+        $('.inputventana13').show();
+    });
+    $('.inputpopup13').click(function () {
+        $('.inputventana13').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".imputDisable").click(function () {
+        $('.inputventana14').show();
+    });
+    $('.inputpopup14').click(function () {
+        $('.inputventana14').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".imputDisable").click(function () {
+        $('.inputventana15').show();
+    });
+    $('.inputpopup15').click(function () {
+        $('.inputventana15').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".imputDisable").click(function () {
+        $('.inputventana16').show();
+    });
+    $('.inputpopup16').click(function () {
+        $('.inputventana16').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".imputDisable").click(function () {
+        $('.inputventana17').show();
+    });
+    $('.inputpopup17').click(function () {
+        $('.inputventana17').hide();
+    });
+});
+
+$(window).load(function () {
+    $(".imputDisable").click(function () {
+        $('.inputventana18').show();
+    });
+    $('.inputpopup18').click(function () {
+        $('.inputventana18').hide();
     });
 });
