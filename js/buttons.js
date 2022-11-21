3b425ed6bddd55f8bebed316f5329fae1e4e16ce
JosueRC02 <jretanacespedes@gmail.com>
2022-11-20 22:48:02 -0600

    botones completados

--- js/buttons.js
+++ js/buttons.js
@@ -97,3 +97,143 @@ $(window).load(function () {
         $('.hover_bkgr_fricc10').hide();
     });
 });
+
+//Boton Verde Borde
+$(window).load(function () {
+    $(".button11").click(function () {
+        $('.hover_bkgr_fricc11').show();
+    });
+    $('.popupCloseButton11').click(function () {
+        $('.hover_bkgr_fricc11').hide();
+    });
+});
+
+//Boton Azul Borde
+$(window).load(function () {
+    $(".button12").click(function () {
+        $('.hover_bkgr_fricc12').show();
+    });
+    $('.popupCloseButton12').click(function () {
+        $('.hover_bkgr_fricc12').hide();
+    });
+});
+
+//Boton Rojo Borde
+$(window).load(function () {
+    $(".button13").click(function () {
+        $('.hover_bkgr_fricc13').show();
+    });
+    $('.popupCloseButton13').click(function () {
+        $('.hover_bkgr_fricc13').hide();
+    });
+});
+
+//Boton Gris Borde
+$(window).load(function () {
+    $(".button14").click(function () {
+        $('.hover_bkgr_fricc14').show();
+    });
+    $('.popupCloseButton14').click(function () {
+        $('.hover_bkgr_fricc14').hide();
+    });
+});
+
+//Boton Negro Borde
+$(window).load(function () {
+    $(".button15").click(function () {
+        $('.hover_bkgr_fricc15').show();
+    });
+    $('.popupCloseButton15').click(function () {
+        $('.hover_bkgr_fricc15').hide();
+    });
+});
+
+//Boton Hover Verde
+$(window).load(function () {
+    $(".button16").click(function () {
+        $('.hover_bkgr_fricc16').show();
+    });
+    $('.popupCloseButton16').click(function () {
+        $('.hover_bkgr_fricc16').hide();
+    });
+});
+
+//Boton Hover Azul
+$(window).load(function () {
+    $(".button17").click(function () {
+        $('.hover_bkgr_fricc17').show();
+    });
+    $('.popupCloseButton17').click(function () {
+        $('.hover_bkgr_fricc17').hide();
+    });
+});
+
+//Boton Hover Rojo
+$(window).load(function () {
+    $(".button18").click(function () {
+        $('.hover_bkgr_fricc18').show();
+    });
+    $('.popupCloseButton18').click(function () {
+        $('.hover_bkgr_fricc18').hide();
+    });
+});
+
+//Boton Hover Gris
+$(window).load(function () {
+    $(".button19").click(function () {
+        $('.hover_bkgr_fricc19').show();
+    });
+    $('.popupCloseButton19').click(function () {
+        $('.hover_bkgr_fricc19').hide();
+    });
+});
+
+//Boton Hover Negro
+$(window).load(function () {
+    $(".button20").click(function () {
+        $('.hover_bkgr_fricc20').show();
+    });
+    $('.popupCloseButton20').click(function () {
+        $('.hover_bkgr_fricc20').hide();
+    });
+});
+
+//Boton DropDown
+$(window).load(function () {
+    $(".dropbtn").click(function () {
+        $('.hover_bkgr_fricc21').show();
+    });
+    $('.popupCloseButton21').click(function () {
+        $('.hover_bkgr_fricc21').hide();
+    });
+});
+
+//Boton Small
+$(window).load(function () {
+    $(".button22").click(function () {
+        $('.hover_bkgr_fricc22').show();
+    });
+    $('.popupCloseButton22').click(function () {
+        $('.hover_bkgr_fricc22').hide();
+    });
+});
+
+//Boton Medium
+$(window).load(function () {
+    $(".button23").click(function () {
+        $('.hover_bkgr_fricc23').show();
+    });
+    $('.popupCloseButton23').click(function () {
+        $('.hover_bkgr_fricc23').hide();
+    });
+});
+
+//Boton Large
+$(window).load(function () {
+    $(".button24").click(function () {
+        $('.hover_bkgr_fricc24').show();
+    });
+    $('.popupCloseButton24').click(function () {
+        $('.hover_bkgr_fricc24').hide();
+    });
+});
